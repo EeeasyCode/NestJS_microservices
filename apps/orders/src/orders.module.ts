be96ac295990dd3986e568795eeb59a874daad8d
EeeasyCode <eeeasycode@gmail.com>
2023-12-03 18:14:48 +0900

    [FEAT] orders modules 설정

--- apps/orders/src/orders.module.ts
+++ apps/orders/src/orders.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from '@app/common';
+
+import Joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [
+    DatabaseModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+      }),
+      envFilePath: './apps/orders/.env',
+    }),
+],
   controllers: [OrdersController],
   providers: [OrdersService],
 })
