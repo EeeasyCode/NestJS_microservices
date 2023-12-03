885073f26bb4b358d7f941686bdac276f8e218d7
EeeasyCode <eeeasycode@gmail.com>
2023-12-03 17:56:36 +0900

    [FEAT] db 설정

--- libs/common/src/database/abstract.schema.ts
+++ libs/common/src/database/abstract.schema.ts
@@ -5,5 +5,5 @@ import { SchemaTypes, Types } from 'mongoose';
 @Schema()
 export class AbstractDocument {
   @Prop({ type: SchemaTypes.ObjectId })
-  _id: Types.ObjectId;
+  _id: Types.ObjectId; // mongoDB의 식별자
 }
