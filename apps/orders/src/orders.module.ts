0e8a4b418ce706eaf6393b97c9882abab9d81a80
EeeasyCode <eeeasycode@gmail.com>
2023-12-03 18:32:05 +0900

    [FIX] abstract repository 수정

--- apps/orders/src/orders.module.ts
+++ apps/orders/src/orders.module.ts
@@ -1,14 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+import { DatabaseModule } from '@app/common';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
-import { ConfigModule } from '@nestjs/config';
-import { DatabaseModule } from '@app/common';
-
-import Joi from 'joi';
+import { OrdersRepository } from './orders.repository';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Order, OrderSchema } from './schemas/orders.schema';
 
 @Module({
   imports: [
-    DatabaseModule,
     ConfigModule.forRoot({
       isGlobal: true,
       validationSchema: Joi.object({
@@ -16,8 +17,10 @@ import Joi from 'joi';
       }),
       envFilePath: './apps/orders/.env',
     }),
-],
+    DatabaseModule,
+    MongooseModule.forFeature([{ name: Order.name, schema: OrderSchema }]),
+  ],
   controllers: [OrdersController],
-  providers: [OrdersService],
+  providers: [OrdersService, OrdersRepository],
 })
 export class OrdersModule {}
